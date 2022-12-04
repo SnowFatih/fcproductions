b4bc17a602be06c3d1c4e84c212bbeb9641303fd
FatihSnow <fatihhcakici@gmail.com>
2022-12-04 17:43:47 +0100

    favicon test

--- src/lib/generateRssFeed.js
+++ src/lib/generateRssFeed.js
@@ -18,8 +18,8 @@ export async function generateRssFeed() {
     author,
     id: siteUrl,
     link: siteUrl,
-    image: `https://fcprods55.surge.sh/favicon.ico`,
-    favicon: `https://fcprods55.surge.sh/favicon.ico`,
+    image: `${siteUrl}/favicon.ico`,
+    favicon: `${siteUrl}/favicon.ico`,
     copyright: `All rights reserved ${new Date().getFullYear()}`,
     feedLinks: {
       rss2: `${siteUrl}/rss/feed.xml`,
